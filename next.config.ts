415a0aaefddffc2a5e8bc906a559842b5a293dc7
Talesoftoba <Tobaking4@gmail.com>
2026-02-19 09:01:59 +0100

    nextconfig.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-/** @type {NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    // Whitelist the Supabase hostname for external images
-    domains: ["ylgbdoxpnffhriorrkah.supabase.co"], // replace with your Supabase project host
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ylgbdoxpnffhriorrkah.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 };
 
